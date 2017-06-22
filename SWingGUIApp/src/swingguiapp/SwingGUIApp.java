c33ec7fcfb904a4757b38a93991d7087a19d2f0b
Muhammad Tayyab Sheikh <cstayyab@gmail.com>
2017-06-23 02:44:39 +0500

    Pccking up the Season
    Due to change of Plan the season was ended.

--- SWingGUIApp/src/swingguiapp/SwingGUIApp.java
+++ SWingGUIApp/src/swingguiapp/SwingGUIApp.java
@@ -9,7 +9,7 @@ import javax.swing.*;
  * @author Muhammad Tayyab Sheikh <cstayyab@gmail.com>
  * @version 3.0
  */
-public class SwingGUIApp implements ActionListener {
+public class SwingGUIApp {
 
     public static void main(String[] args) {
         JFrame myWindow = new JFrame("This is the title of JFrame");
@@ -24,15 +24,5 @@ public class SwingGUIApp implements ActionListener {
         myContentPane.add(button2);
         JButton button3 = new JButton("Button 3");
         myContentPane.add(button3);
-        button1.setActionCommand("Button1");
-        button2.setActionCommand("Button2");
-        button3.setActionCommand("Button3");
-
-    }
-
-    public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand().equalsIgnoreCase("Button1")) {
-
-        }
     }
 }
